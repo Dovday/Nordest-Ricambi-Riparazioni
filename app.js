53cec1bef79b3afdeaf1afefd58e0eaf901f6046
Dovday <dvdcarlomagno@gmail.com>
2022-03-05 10:52:03 +0100

    v.05.03

--- app.js
+++ app.js
@@ -32,12 +32,22 @@ $(document).ready(function () {
 
 // SMOOTH SCROLLING WHEN CLICKING ANCHOR LINK
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+
+// support for old browsers
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
