aac30e0435d841ddb3f8fc64c72b307328fe3d1e
Dovday <dvdcarlomagno@gmail.com>
2022-03-09 21:17:55 +0100

    v.09.03

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ $(window).scroll(function() {
 
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+const anchorLink = document.querySelectorAll('.primary-navigation a');
 
 $(document).ready(function () {
     $(document).click(function (event) {
@@ -51,3 +52,44 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+// =============================================
+// to get first visable element to update navbar
+// =============================================
+
+// Get all elements that have an ID defined
+const elem = document.querySelectorAll("*[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+
+    const current = document.querySelector("#main-page")
+    const bounding = current.getBoundingClientRect();
+    const elem_id = current.getAttribute("id");
+    console.log(bounding);
+
+    if(bounding.top <= 1){
+        document.querySelector(".primary-navigation a[href*=" + elem_id + "]").classList.add("active");
+    } else {
+        document.querySelector(".primary-navigation a[href*=" + elem_id + "]").classList.remove("active");
+    }
+
+  
+//   // Now we loop through sections to get height, top and ID values for each
+//   elem.forEach(current => {
+//     const bounding = current.getBoundingClientRect();
+//     const elem_id = current.getAttribute("id");
+//     console.log(bounding);
+//     /*
+//     - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+//     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+//     */
+//     if(bounding < 0){
+//       document.querySelector(".primary-navigation a[href*=" + elem_id + "]").classList.add("active");
+//     } else {
+//       document.querySelector(".primary-navigation a[href*=" + elem_id + "]").classList.remove("active");
+//     }
+//   });
+}
